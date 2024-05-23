ffc901ab41b3a5f8ef577b29770fab3cf73e8c49
BlinkDany <111986563+BlinkDany@users.noreply.github.com>
2024-05-22 20:01:22 -0500

    Revert "Add files via upload"
    This reverts commit e3cf80dfcbcb179932857dd43da0a220bf8b91fe.

--- ProyectoUsuario/src/main/java/com/tendencias/demo/Dao1/IUsuarioDao.java
+++
@@ -1,9 +0,0 @@
-package com.tendencias.demo.Dao1;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.tendencias.demo.Entity1.Usuario;
-
-public interface IUsuarioDao extends CrudRepository<Usuario, Long>{
-
-}
